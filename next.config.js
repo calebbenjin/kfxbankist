821293ef8e8fcbc946692c8353b93de2bb41ff06
calebbenjin <calebben94@gmail.com>
2023-02-11 15:50:21 +0100

    env

--- next.config.js
+++ next.config.js
@@ -15,8 +15,8 @@ const nextConfig = withPWA({
   env: {
     REACT_APP_API_URL: 'https://kfxbankist-api-production.up.railway.app/api/v1/users/',
     // REACT_APP_API_URL: 'http://localhost:8080/api/v1/users/',
-    // NEXT_APP_API_URL: 'https://kfxbankist.vercel.app/api',
-    NEXT_APP_API_URL: 'http://localhost:3000/api',
+    NEXT_APP_API_URL: 'https://kfxbankist.vercel.app/api',
+    // NEXT_APP_API_URL: 'http://localhost:3000/api',
     REACT_APP_CURRENCY_API: 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies'
   }
 });
